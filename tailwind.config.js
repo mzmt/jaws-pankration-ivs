bec3ce5bd88805d95ee415aa07c2be3f27384ee4
Mizumoto Ryohei <mrcrius1355@gmail.com>
2021-10-31 22:05:38 +0900

    add tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // purge: 本番環境で使わないcssをパージする
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
