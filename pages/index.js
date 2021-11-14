518ea60de293e1c6624ed4ea0f5470b650a83c26
松井英俊 <dev.matsuihidetoshi@gmail.com>
2021-11-14 20:44:04 +0900

    [Add]Video.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Script from 'next/script'
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
 
 export default function Home() {
   return (
@@ -10,16 +12,29 @@ export default function Home() {
         <title>JAWS PANKRATION 2021</title>
         <meta name="description" content="JAWS PANKRATION 2021" />
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://player.live-video.net/1.2.0/amazon-ivs-videojs-tech.min.js"></script>
+        <script src="https://player.live-video.net/1.2.0/amazon-ivs-quality-plugin.min.js"></script>
       </Head>
       <Script
         src = "https://player.live-video.net/1.5.0/amazon-ivs-player.min.js"
         strategy="afterInteractive"
         onLoad = {() => {
           if (IVSPlayer.isPlayerSupported) {
-            const player = IVSPlayer.create();
-            player.attachHTMLVideoElement(document.getElementById('video-player'));
-            player.load(process.env.IVS_STREAM_URL);
-            player.play();
+            registerIVSTech(videojs);
+            registerIVSQualityPlugin(videojs);
+
+            const player = videojs(
+              "video-player",
+              {
+                techOrder: ["AmazonIVS"],
+              },
+              () => {
+                console.log("Player is ready to use!");
+
+                player.enableIVSQualityPlugin();
+                player.src(process.env.IVS_STREAM_URL);
+              }
+            );
           }
         }}
       />
@@ -27,7 +42,7 @@ export default function Home() {
         <Image src="/jawspankration_main-image.png" alt="JAWS PANKRATION header" width={1440} height={230} />
       </header>
       <main className={styles.main}>
-        <video id="video-player" width="1000" height="700" controls playsInline className="px-8"></video>
+        <video id="video-player" width="1000" height="700" controls playsInline autoPlay className="px-8 video-js vjs-big-play-centered"></video>
       </main>
 
       {/* <footer className={styles.footer}>
